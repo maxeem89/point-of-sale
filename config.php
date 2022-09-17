12b3acd84e460827a4f35ebe06ae70d2df828544
maxeem <maxeemkh1989@gmail.com>
2022-09-17 08:08:53 +0300

    test

--- config.php
+++ config.php
@@ -6,9 +6,9 @@
     // many to many // Posts to Tags
 
 // Database configrations
-define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '123456789');
+define('DB_HOST', 'db-mysql-fra1-59186-do-user-12450665-0.b.db.ondigitalocean.com:25060');
+define('DB_USERNAME', 'doadmin');
+define('DB_PASSWORD', 'AVNS_a0y6ekLrPO4pWbKMDen');
 define('DB_NAME', 'point_of_sales');
 
 
